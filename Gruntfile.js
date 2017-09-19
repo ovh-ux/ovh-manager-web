dcc24688f1fc5e8b9f6a07c924966785d60d866e
rfougier <romain.fougier@corp.ovh.com>
2017-09-19 16:51:38 -0400

    :sparkles: chore(*): Add oui-angular module

--- Gruntfile.js
+++ Gruntfile.js
@@ -572,7 +572,8 @@ module.exports = function (grunt) {
                             "ovh-angular-toaster",
                             "ovh-common-style",
                             "ngCkeditor",
-                            "moment-picker"
+                            "moment-picker",
+                            "oui"
                         ];
                         _.forEach(assets[target].modules, (module) => {
                             if (regExp.test(module)) {
@@ -693,7 +694,8 @@ module.exports = function (grunt) {
                             "ovh-angular-toaster",
                             "ovh-common-style",
                             "ngCkeditor",
-                            "moment-picker"
+                            "moment-picker",
+                            "oui"
                         ];
 
                         _.forEach(assets[target].modules, (module) => {
@@ -746,11 +748,9 @@ module.exports = function (grunt) {
                         MANAGER_URLS: constants[target].MANAGER_URLS,
                         HOSTING: constants[target].HOSTING,
                         DOMAIN: constants[target].DOMAIN,
-                        NO_AUTORENEW_COUNTRIES: constants[target]
-                            .NO_AUTORENEW_COUNTRIES,
+                        NO_AUTORENEW_COUNTRIES: constants[target].NO_AUTORENEW_COUNTRIES,
                         WEBSITE_URLS: constants[target].website_url,
-                        new_bdd_user_grant_options: constants[target]
-                            .new_bdd_user_grant_options,
+                        new_bdd_user_grant_options: constants[target].new_bdd_user_grant_options,
                         OVHGuides: constants[target].OVHGuides
                     },
                     LANGUAGES: constants[target].LANGUAGES,
