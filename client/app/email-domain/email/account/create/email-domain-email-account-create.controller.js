04b70e803a36696a6b88900549f42638ce715287
rfougier <romain.fougier@corp.ovh.com>
2017-09-20 09:31:25 -0400

    :ambulance: fix(emails): Remove useless all guides link

--- client/app/email-domain/email/account/create/email-domain-email-account-create.controller.js
+++ client/app/email-domain/email/account/create/email-domain-email-account-create.controller.js
@@ -81,8 +81,6 @@ angular.module("App").controller(
             this.$scope.createAccount = () => this.createAccount();
 
             this.User.getUrlOf("guides").then((guides) => {
-                this.allGuides = _.get(guides, "emailsConfiguration");
-
                 this.createGuide("MAC", "", _.get(guides, "emailsConfigurationAuto"), "assets/images/logos/iOS9.png");
                 this.createGuide("MAC", "El capitan", _.get(guides, "emailsConfigurationMacElCapitain"), "assets/images/logos/iOS9.png");
                 this.createGuide("MAC", "Mavericks / Yosemite", _.get(guides, "emailsConfigurationMacMavericksAndYosemite"), "assets/images/logos/iOS9.png");
