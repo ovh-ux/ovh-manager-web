0092f650c2a0a44958ec2b861a8b5d092238b917
rfougier <romain.fougier@corp.ovh.com>
2017-10-13 13:50:42 -0400

    :lipstick: style(order): update order button color

--- client/app/components/ovh-angular-actions-menu/ovh-angular-actions-menu.module.js
+++
@@ -1,5 +0,0 @@
-angular
-    .module("App")
-    .config((actionsMenuProvider) => {
-        actionsMenuProvider.setTranslationPath("bower_components/ovh-angular-actions-menu/dist/ovh-angular-actions-menu");
-    });
