abc4b5809246029d06b21b2decabef63d6a0d72e
DakuTenshi <dakutenshi@gmail.com>
2017-10-31 11:06:44 +0100

    :lipstick: style(manager-navbar): refactored

--- client/app/components/manager-navbar/manager-navbar.module.js
+++ client/app/components/manager-navbar/manager-navbar.module.js
@@ -1,96 +1,141 @@
-angular.module("App").run((LANGUAGES, managerNavbar, User, constants, translator, ssoAuthentication, OtrsPopupService) => {
-
-    User.getUser().then((user) => {
-        const universes = Object.keys(constants.MANAGER_URLS)
-                                .map((k) => ({
-                                    universe: k,
-                                    label: translator.tr(`universe_univers-${k}_name`),
-                                    url: constants.MANAGER_URLS[k]
-                                }))
-                                .filter((u) => u.universe === "partners" ? user.ovhSubsidiary === "FR" : true);
-
-        managerNavbar.setExternalLinks(universes);
-
-        const currentSubsidiaryURLs = constants.urls[user.ovhSubsidiary];
-
-        const subLinks = [{
-            label: translator.tr("common_menu_support_new_ticket"),
-            click: () => {
-                if (!OtrsPopupService.isLoaded()) {
-                    OtrsPopupService.init();
-                } else {
-                    OtrsPopupService.toggle();
-                }
-            }
-        }, {
-            label: translator.tr("common_menu_support_list_ticket"),
-            url: "#/support"
-        }];
-
-        if (currentSubsidiaryURLs != null) {
-            subLinks.unshift({
-                label: translator.tr("common_menu_support_all_guides"),
-                url: currentSubsidiaryURLs.guides.home
+angular
+    .module("App")
+    .run((LANGUAGES, managerNavbar, User, constants, translator, ssoAuthentication, OtrsPopupService) => {
+        return User.getUser()
+            .then((currentUser) => {
+                setMenuItemssToOtherUniverses(currentUser);
+                setGeneralMenuItems(currentUser);
             });
 
-            subLinks.push({
-                label: translator.tr("common_menu_support_telephony_contact"),
-                url: currentSubsidiaryURLs.support_contact
-            });
+        function setMenuItemssToOtherUniverses (currentUser) {
+            const universeMenuItems = Object.keys(constants.MANAGER_URLS)
+                .map((universeName) => ({
+                    universe: universeName,
+                    label: translator.tr(`universe_univers-${universeName}_name`),
+                    url: constants.MANAGER_URLS[universeName]
+                }))
+                .filter((universeMenuItem) => universeMenuItem.universe === "partners" ? currentUser.ovhSubsidiary === "FR" : true);
+
+            managerNavbar.setExternalLinks(universeMenuItems);
         }
 
-        const internalLinks = [{
-            label: translator.tr("common_menu_support_assistance"),
-            subLinks
-        },
-        {
-            label: translator.tr("global_billing"),
-            url: constants.BILLING_URL
-        },
-        {
-            label: (function () {
-                const selectedLang = _.find(LANGUAGES, { value: translator.getLanguage() });
-                return _.get(selectedLang, "name");
-            })(),
-            subLinks: (function () {
-                const subLinksLang = [];
-                angular.forEach(LANGUAGES, (lang) => {
-                    subLinksLang.push({
-                        label: lang.name,
-                        click: () => {
-                            localStorage["univers-selected-language"] = lang.value;
-                            window.location.reload();
-                        },
-                        lang: _.chain(lang.value).words().head().value()
-                    });
-                });
-                return subLinksLang;
-            })()
-        },
-        {
-            label: `${user.firstName} ${user.lastName} (${user.nichandle})`,
-            subLinks: [
+        function setGeneralMenuItems (currentUser) {
+            const generalMenuItems = buildGeneralMenuItems(currentUser);
+
+            managerNavbar.setInternalLinks(generalMenuItems);
+        }
+
+        function buildGeneralMenuItems (currentUser) {
+            const assistanceMenuItem = buildAssistanceMenuItem(currentUser);
+
+            const generalMenuItems = [
+                assistanceMenuItem,
                 {
-                    label: translator.tr("global_account"),
-                    url: "#/useraccount/infos"
-                }, {
                     label: translator.tr("global_billing"),
-                    url: "#/billing/history"
-                }, {
-                    label: translator.tr("global_renew"),
-                    url: "#/billing/autoRenew"
-                }, {
-                    label: translator.tr("global_orders"),
-                    url: "#/billing/orders"
-                }, {
-                    label: translator.tr("global_logout"),
-                    click: () => {
-                        ssoAuthentication.logout();
+                    url: constants.BILLING_URL
+                }
+            ];
+
+            const currentLanguageName = getCurrentLanguageName();
+            const currentLanguageDetectedCorrectly = _(currentLanguageName).isString();
+
+            if (currentLanguageDetectedCorrectly) {
+                generalMenuItems.push({
+                    label: currentLanguageName,
+                    subLinks: getLanguageChangingMenuObject()
+                });
+            }
+
+            generalMenuItems.push({
+                label: `${currentUser.firstName} ${currentUser.lastName} (${currentUser.nichandle})`,
+                subLinks: [
+                    {
+                        label: translator.tr("global_account"),
+                        url: "#/useraccount/infos"
+                    }, {
+                        label: translator.tr("global_billing"),
+                        url: "#/billing/history"
+                    }, {
+                        label: translator.tr("global_renew"),
+                        url: "#/billing/autoRenew"
+                    }, {
+                        label: translator.tr("global_orders"),
+                        url: "#/billing/orders"
+                    }, {
+                        label: translator.tr("global_logout"),
+                        click: () => {
+                            ssoAuthentication.logout();
+                        }
+                    }
+                ]
+            });
+
+            return generalMenuItems;
+        }
+
+        function buildAssistanceMenuItem (currentUser) {
+            const currentSubsidiaryURLs = constants.urls[currentUser.ovhSubsidiary];
+
+            const assistanceMenuItems = [];
+            if (_(currentSubsidiaryURLs).has("guides.home")) {
+                assistanceMenuItems.push({
+                    label: translator.tr("common_menu_support_all_guides"),
+                    url: currentSubsidiaryURLs.guides.home
+                });
+            }
+
+            assistanceMenuItems.push({
+                label: translator.tr("common_menu_support_new_ticket"),
+                click: () => {
+                    if (!OtrsPopupService.isLoaded()) {
+                        OtrsPopupService.init();
+                    } else {
+                        OtrsPopupService.toggle();
                     }
                 }
-            ]
-        }];
+            });
 
-        managerNavbar.setInternalLinks(internalLinks);
+            assistanceMenuItems.push({
+                label: translator.tr("common_menu_support_list_ticket"),
+                url: "#/support"
+            });
+
+            if (_(currentSubsidiaryURLs).has("support_contact")) {
+                assistanceMenuItems.push({
+                    label: translator.tr("common_menu_support_telephony_contact"),
+                    url: currentSubsidiaryURLs.support_contact
+                });
+            }
+
+            return {
+                label: translator.tr("common_menu_support_assistance"),
+                subLinks: assistanceMenuItems
+            };
+        }
+
+        function getCurrentLanguageName () {
+            const currentLanguage = _(LANGUAGES).find({ value: translator.getLanguage() });
+            const currentLanguageName = _(currentLanguage).get("name");
+
+            return currentLanguageName;
+        }
+
+        function getLanguageChangingMenuObject () {
+            const languageChangingMenuObject = _(LANGUAGES)
+                .filter((language) => _(language).has("name", "value"))
+                .map((language) => ({
+                    label: language.name,
+                    click: () => {
+                        localStorage["univers-selected-language"] = language.value;
+                        window.location.reload();
+                    },
+                    lang: _(language.value).chain()
+                        .words()
+                        .head()
+                        .value()
+                }))
+                .value();
+
+            return languageChangingMenuObject;
+        }
     });
-});
