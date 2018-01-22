eab392116b3b4e0c96602903690afe1ce1d4ee52
euhmeuh <jerome.martin@corp.ovh.com>
2018-01-22 17:49:28 +0100

    :ambulance: hotfix(hosting/database): PR fixes

--- client/app/hosting/database/private-activation/database-private-activation.controller.js
+++ client/app/hosting/database/private-activation/database-private-activation.controller.js
@@ -10,10 +10,14 @@ angular.module("App").controller(
             this.Alerter = Alerter;
             this.PrivateDatabase = PrivateDatabase;
 
+            this.$scope.activateDatabase = () => this.activateDatabase();
+        }
+
+        $onInit () {
             this.host = this.$scope.currentActionData;
             this.versions = [];
             this.loaders = {
-                versions: false
+                retrievingVersions: true
             };
             this.choice = {
                 ram: this.host.offerCapabilities.privateDatabases.length === 1 ?
@@ -21,38 +25,27 @@ angular.module("App").controller(
                 version: null
             };
 
-            this.$scope.activateDatabase = () => this.activateDatabase();
-        }
-
-        $onInit () {
-            if (!this.loaders.versions) {
-                this.loaders.versions = true;
-                this.PrivateDatabase.getAvailableOrderCapacities("classic")
-                    .then((capacities) => {
-                        this.versions = _.get(capacities, "version");
-                        this.choice.version = this.versions.length === 1 ? _.first(this.versions) : null;
-                    })
-                    .catch((err) => {
-                        this.Alerter.alertFromSWS(this.$scope.tr("hosting_dashboard_database_versions_error",
-                                                                 [this.$scope.entryToDelete]),
-                                                  _.get(err, "data", err),
-                                                  this.$scope.alerts.main);
-                    })
-                    .finally(() => {
-                        this.loaders.versions = false;
-                    });
-            }
+            this.PrivateDatabase.getAvailableOrderCapacities("classic")
+                .then((capacities) => {
+                    this.versions = _.get(capacities, "version");
+                    this.choice.version = this.versions && this.versions.length === 1 ? _.first(this.versions) : null;
+                })
+                .catch((err) => {
+                    this.Alerter.alertFromSWS(this.$scope.tr("hosting_dashboard_database_versions_error",
+                                                             [this.$scope.entryToDelete]),
+                                              _.get(err, "data", err),
+                                              this.$scope.alerts.main);
+                })
+                .finally(() => {
+                    this.loaders.retrievingVersions = false;
+                });
         }
 
         activateDatabase () {
-            if (this.loaders.versions) {
-                return;
-            }
-
-            this.loaders.versions = true;
-            this.HostingDatabase.activateDatabasePrivate(this.$stateParams.productId,
-                                                         this.choice.ram.quota.value,
-                                                         this.choice.version)
+            return this.HostingDatabase
+                .activateDatabasePrivate(this.$stateParams.productId,
+                                         this.choice.ram.quota.value,
+                                         this.choice.version)
                 .then(() => {
                     this.$rootScope.$broadcast("hosting.database.sqlPrive");
                     this.Alerter.success(this.$scope.tr("hosting_dashboard_database_active_success"),
