517f25fcb306d47b58fa4ef1bcc048f2f0f09acb
Marie Jones <marie.jones@corp.ovh.com>
2018-04-06 13:07:16 +0200

    :ambulance: fix(ckeditor): Prevent modal dialog conflicts
    Prevent modal dialog focus conflicts on inputs between ckeditor modals and bootstrap modals

--- client/app/app.controller.js
+++ client/app/app.controller.js
@@ -85,6 +85,21 @@ angular.module("App").controller(
                     }
                 }
             });
+
+            // Prevents a bug with CKEditor.
+            // See: https://stackoverflow.com/a/23667151 and includes updates from https://github.com/twbs/bootstrap-sass/blob/5d6b2ebba0c2a5885ce2f0e01e9218db3d3b5e47/assets/javascripts/bootstrap/modal.js#L139
+            this.$timeout(() => {
+                $.fn.modal.Constructor.prototype.enforceFocus = function () {
+                    $(document)
+                        .off("focusin.bs.modal")
+                        .on("focusin.bs.modal", $.proxy((event) => {
+                            const $parent = $(event.target.parentNode);
+                            if (document !== event.target && this.$element[0] !== event.target && !this.$element.has(event.target).length && !$parent.hasClass("cke_dialog_ui_input_select") && !$parent.hasClass("cke_dialog_ui_input_text")) {
+                                this.$element.trigger("focus");
+                            }
+                        }, this));
+                };
+            });
         }
 
         setAction (action, data) {
