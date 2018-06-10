9c9d95539540c75247326ed03a0e63db6cdeca0c
Frederic Espiau <frederic.espiau@corp.ovh.com>
2018-06-10 19:03:52 +0200

    fix: chart now works better

--- client/app/private-database/metrics/private-database-metrics.constant.js
+++ client/app/private-database/metrics/private-database-metrics.constant.js
@@ -1,68 +1,179 @@
-angular.module("App").constant("PRIVATE_DATABASE_METRICS", {
-    chart: {
-        type: "line",
-        data: {
-            datasets: []
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            legend: {
-                position: "bottom",
-                display: true
+angular
+    .module("App")
+    .constant("PRIVATE_DATABASE_METRICS", {
+        settingsForAllCharts: {
+            type: "line",
+            data: {
+                datasets: []
             },
-            elements: {
-                point: {
-                    radius: 0
-                }
-            },
-            tooltips: {
-                mode: "label",
-                intersect: false,
-                callbacks: {
-                    title (data) {
-                        return moment(_.get(_.first(data), "xLabel")).fromNow();
-                    }
-                }
-            },
-            pan: {
-                enabled: true,
-                mode: "xy"
-            },
-            zoom: {
-                enabled: true,
-                mode: "xy",
-                limits: {
-                    max: 10,
-                    min: 0.5
-                }
-            },
-            scales: {
-                yAxes: [{
-                    display: true,
-                    position: "left",
-                    scaleLabel: {
-                        display: true
-                    },
-                    gridLines: {
-                        drawBorder: true,
-                        display: true
-                    }
-                }],
-                xAxes: [{
-                    type: "time",
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
                     position: "bottom",
-                    gridLines: {
-                        drawBorder: true,
-                        display: false
-                    },
-                    time: {
-                        displayFormats: {
-                            hour: "LT"
-                        }
+                    display: true
+                },
+                elements: {
+                    point: {
+                        radius: 0
                     }
-                }]
+                }
             }
-        }
-    }
-});
+        },
+        settingsForAllSeries: {
+            dataset: {
+                fill: true,
+                borderWidth: 1
+            }
+        },
+        specificDatabaseVersionChartSelection: {
+            mysql_55: ["memoryUsages"]
+        },
+        specificChartSettings: [
+            {
+                chartName: "memoryUsages",
+                dataFromAPIIndex: 0,
+                options: {
+                    tooltips: {
+                        mode: "label",
+                        intersect: false,
+                        callbacks: {
+                            title: (items) => _(items).chain()
+                                .first()
+                                .get("xLabel")
+                                .value(),
+                            label: (item) => `${item.yLabel}%`
+                        }
+                    },
+                    scales: {
+                        yAxes: [{
+                            type: "linear",
+                            display: true,
+                            position: "left",
+                            scaleLabel: {
+                                display: true
+                            },
+                            gridLines: {
+                                drawBorder: true,
+                                display: true
+                            },
+                            ticks: {
+                                suggestedMin: 0,
+                                suggestedMax: 100,
+                                callback: (label) => `${label}%`
+                            }
+                        }],
+                        xAxes: [{
+                            type: "time",
+                            position: "bottom",
+                            gridLines: {
+                                drawBorder: true,
+                                display: false
+                            },
+                            time: {
+                                displayFormats: {
+                                    hour: "LT"
+                                }
+                            }
+                        }]
+                    }
+                }
+            },
+            {
+                chartName: "activeConnections",
+                dataFromAPIIndex: 1,
+                options: {
+                    tooltips: {
+                        mode: "label",
+                        intersect: false,
+                        callbacks: {
+                            title: (items) => _(items).chain()
+                                .first()
+                                .get("xLabel")
+                                .value(),
+                            label: (item) => `${item.yLabel}`
+                        }
+                    },
+                    scales: {
+                        yAxes: [{
+                            type: "linear",
+                            display: true,
+                            position: "left",
+                            scaleLabel: {
+                                display: true
+                            },
+                            gridLines: {
+                                drawBorder: true,
+                                display: true
+                            },
+                            ticks: {
+                                suggestedMin: 0,
+                                stepSize: 1
+                            }
+                        }],
+                        xAxes: [{
+                            type: "time",
+                            position: "bottom",
+                            gridLines: {
+                                drawBorder: true,
+                                display: false
+                            },
+                            time: {
+                                displayFormats: {
+                                    hour: "LT"
+                                }
+                            }
+                        }]
+                    }
+                }
+            },
+            {
+                chartName: "queryExecutionTimes",
+                dataFromAPIIndex: 2,
+                options: {
+                    tooltips: {
+                        mode: "label",
+                        intersect: false,
+                        callbacks: {
+                            title: (items) => _(items).chain()
+                                .first()
+                                .get("xLabel")
+                                .value(),
+                            label: (item) => `${item.yLabel}ms`
+                        }
+                    },
+                    scales: {
+                        yAxes: [{
+                            type: "linear",
+                            display: true,
+                            position: "left",
+                            scaleLabel: {
+                                display: true
+                            },
+                            gridLines: {
+                                drawBorder: true,
+                                display: true
+                            },
+                            ticks: {
+                                suggestedMin: 0,
+                                callback: (label) => `${label}ms`
+                            }
+                        }],
+                        xAxes: [{
+                            type: "time",
+                            position: "bottom",
+                            gridLines: {
+                                drawBorder: true,
+                                display: false
+                            },
+                            time: {
+                                displayFormats: {
+                                    hour: "LT"
+                                }
+                            }
+                        }]
+                    }
+                }
+            }
+        ]
+    });
