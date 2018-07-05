1eb560ce923ebd4d41c60be600fa602d99d4ee3e
Jean-Daniel Pearson <1476965+JDownloader@users.noreply.github.com>
2018-07-05 03:40:51 -0400

    feat(navbar): add notifications placeholder translations (#448)

--- client/app/components/navbar/navbar-notification.service.js
+++ client/app/components/navbar/navbar-notification.service.js
@@ -21,14 +21,9 @@ class NavbarNotificationService {
     }
 
     getSubLinks () {
-        return this.getMessages().then((messages) => messages.map((message) => this.convertSubLink(message))).catch(() => [{
-            id: "0",
-            date: moment(),
-            time: this._formatTime(moment()),
-            level: "error",
-            subject: this.translator.tr("common_navbar_notification_error_subject"),
-            description: this.translator.tr("common_navbar_notification_error")
-        }]);
+        return this.getMessages()
+            .then((messages) => messages.map((message) => this.convertSubLink(message)))
+            .catch(() => undefined);
     }
 
     /* eslint-disable class-methods-use-this */
