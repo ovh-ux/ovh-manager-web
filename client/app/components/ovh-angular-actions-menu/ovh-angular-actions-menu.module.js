3493181ddf11cd35fee35ed3f267dfd8bd80ec03
Jérôme Martin <jerome.martin@corp.ovh.com>
2018-09-04 17:23:56 +0200

    fix(yarn): yet another path fix

--- client/app/components/ovh-angular-actions-menu/ovh-angular-actions-menu.module.js
+++ client/app/components/ovh-angular-actions-menu/ovh-angular-actions-menu.module.js
@@ -1,5 +1,5 @@
 angular
   .module('App')
   .config((actionsMenuProvider) => {
-    actionsMenuProvider.setTranslationPath('bower_components/ovh-angular-actions-menu/dist/ovh-angular-actions-menu');
+    actionsMenuProvider.setTranslationPath('node_modules/ovh-angular-actions-menu/dist/ovh-angular-actions-menu');
   });
