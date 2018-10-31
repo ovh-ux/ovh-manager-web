60cc1b3c55b2a90a8c452277167c37ccceb3db97
Cyrille Bourgois <56223+cbourgois@users.noreply.github.com>
2018-10-31 13:12:55 +0100

    refactor(components.cron*): move to webUniverseComponents
    ref: OM-152

--- client/app/components/cron/cronEditor.directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('directives').directive('cronEditor', () => ({
-  restrict: 'E',
-  replace: true,
-  scope: {
-    crontabObject: '=',
-  },
-  templateUrl: 'components/cron/cronEdit.html',
-  controller: 'cronEditorCtrl',
-}));
--- client/app/components/cron/cronSimpleSummary.directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('directives').directive('cronSimpleSummary', () => ({
-  restrict: 'E',
-  replace: true,
-  scope: {
-    crontabObject: '=',
-  },
-  templateUrl: 'components/cron/cronSimpleSummary.html',
-  controller: 'cronSimpleSummaryCtrl',
-}));
