ba9e7a2f7777cae22404bb255d426636f36ed64c
Cyrille Bourgois <cyrille.bourgois@gmail.com>
2018-11-05 10:15:12 +0100

    refactor(components.hash): remove unused component

--- client/app/components/hash/orderObjectBy.js
+++
@@ -1,7 +0,0 @@
-angular.module('filters').filter('orderObjectBy', () => (arr, prop) => {
-  _.each(arr, (item, key) => {
-    item.key = key; // eslint-disable-line no-param-reassign
-  });
-
-  return _.sortBy(arr, prop);
-});
