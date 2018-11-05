5ba4f972caf5fc30e42a013271f2e162903831ac
Marie JONES <marie.jones@corp.ovh.com>
2018-11-05 09:22:16 +0100

    feat(navbar): add tracking for assistance and notifications

--- client/app/components/navbar/navbar-notification.service.js
+++ client/app/components/navbar/navbar-notification.service.js
@@ -1,13 +1,14 @@
 class NavbarNotificationService {
   constructor(
     $interval, $q, $translate,
-    Alerter, constants, OvhApiNotificationAapi,
+    Alerter, atInternet, constants, OvhApiNotificationAapi,
     UNIVERSE,
   ) {
     this.$interval = $interval;
     this.$q = $q;
     this.$translate = $translate;
     this.alerter = Alerter;
+    this.atInternet = atInternet;
     this.constants = constants;
     this.OvhApiNotificationAapi = OvhApiNotificationAapi;
     this.UNIVERSE = UNIVERSE;
@@ -98,7 +99,13 @@ class NavbarNotificationService {
         title: this.$translate.instant('common_navbar_notification_title'),
         iconClass: 'icon-notifications',
         limitTo: 10,
-        onClick: () => this.acknowledgeAll(),
+        onClick: () => {
+          this.acknowledgeAll();
+          this.atInternet.trackClick({
+            name: 'notifications',
+            type: 'action',
+          });
+        },
         subLinks: sublinks,
         show: true,
       };
