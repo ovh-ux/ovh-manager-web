078ffc18569a1d3b84688a8fc02c6cc3a5813e8a
Cyrille Bourgois <cyrille.bourgois@gmail.com>
2018-11-07 19:35:17 +0100

    refactor(components.duration): move to webUniverseComponents
    ref: OM-172

--- client/app/webUniverseComponents/index.js
+++ client/app/webUniverseComponents/index.js
@@ -3,6 +3,7 @@ import angular from 'angular';
 import wucConverter from './converter';
 import wucCron from './cron';
 import wucCronValidator from './cron-validator';
+import wucDuration from './duration';
 import wucExpiration from './expiration';
 import wucFileChange from './fileChange';
 import wucFileEditor from './fileEditor';
@@ -17,6 +18,7 @@ angular
     wucConverter,
     wucCron,
     wucCronValidator,
+    wucDuration,
     wucExpiration,
     wucFileChange,
     wucFileEditor,
