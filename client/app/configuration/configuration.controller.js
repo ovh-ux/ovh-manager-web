4b679ace3cb2e2316706a7463f277b66a105af96
FredericEspiau <frederic.espiau@corp.ovh.com>
2018-12-17 17:03:40 +0100

    fix(welcome): change message for FR subsidiary

--- client/app/configuration/configuration.controller.js
+++ client/app/configuration/configuration.controller.js
@@ -4,55 +4,38 @@ angular
     'configurationCtrl',
     class ConfigurationCtrl {
       constructor(
-        $route,
-        $scope,
-
         constants,
         User,
-        WucProducts,
       ) {
-        this.$route = $route;
-        this.$scope = $scope;
-
         this.constants = constants;
         this.User = User;
-        this.WucProducts = WucProducts;
       }
 
       $onInit() {
         this.guides = this.constants.TOP_GUIDES;
 
-        this.currentGuides = [];
-        this.currentTypeOfGuide = null;
-
-        this.selectTypeOfGuide('domainHosting');
+        this.helpCenterURLs = _.omit(
+          _.mapValues(
+            this.constants.urls,
+            'support',
+          ),
+          _.isUndefined,
+        );
 
         return this.User
           .getUser()
-          .then((user) => {
+          .then(({ ovhSubsidiary: subsidiary }) => {
+            this.subsidiary = subsidiary;
+
             this.allGuides = _.get(
               this.constants,
-              `urls.${user.ovhSubsidiary}.guides.all`,
+              `urls.${subsidiary}.guides.all`,
               this.constants.urls.FR.guides.all,
             );
           })
           .catch(() => {
             this.allGuides = this.constants.urls.FR.guides.all;
-          })
-          .then(() => this.unSelectProduct());
-      }
-
-      unSelectProduct() {
-        return this.WucProducts
-          .removeSelectedProduct()
-          .then((p) => {
-            this.$scope.product = p;
           });
-      }
-
-      selectTypeOfGuide(typeOfGuide) {
-        this.currentGuides = this.guides[typeOfGuide];
-        this.currentTypeOfGuide = typeOfGuide;
       }
     },
   );
