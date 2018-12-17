bb0f24d6e570b82f5bfb6499dda08eeb7e330753
FredericEspiau <frederic.espiau@corp.ovh.com>
2018-12-17 15:30:14 +0100

    refactor(configuration): move to Component

--- client/app/configuration/configuration.controller.js
+++ client/app/configuration/configuration.controller.js
@@ -1,51 +1,58 @@
-angular.module('App').controller(
-  'ConfigurationCtrl',
-  class ConfigurationCtrl {
-    constructor(
-      $scope,
-      $route,
-      WucProducts,
-      constants,
-      User,
-    ) {
-      this.$scope = $scope;
-      this.$route = $route;
-      this.WucProducts = WucProducts;
-      this.constants = constants;
-      this.User = User;
-    }
+angular
+  .module('App')
+  .controller(
+    'configurationCtrl',
+    class ConfigurationCtrl {
+      constructor(
+        $route,
+        $scope,
 
-    $onInit() {
-      this.guides = this.constants.TOP_GUIDES;
+        constants,
+        User,
+        WucProducts,
+      ) {
+        this.$route = $route;
+        this.$scope = $scope;
 
-      this.User.getUser()
-        .then((user) => {
-          this.allGuides = _.get(
-            this.constants,
-            `urls.${user.ovhSubsidiary}.guides.all`,
-            this.constants.urls.FR.guides.all,
-          );
-        })
-        .catch(() => {
-          this.allGuides = this.constants.urls.FR.guides.all;
-        });
+        this.constants = constants;
+        this.User = User;
+        this.WucProducts = WucProducts;
+      }
 
-      this.currentGuides = [];
-      this.currentTypeOfGuide = null;
+      $onInit() {
+        this.guides = this.constants.TOP_GUIDES;
 
-      this.unSelectProduct();
-      this.selectTypeOfGuide('domainHosting');
-    }
+        this.currentGuides = [];
+        this.currentTypeOfGuide = null;
 
-    unSelectProduct() {
-      this.WucProducts.removeSelectedProduct().then((p) => {
-        this.$scope.product = p;
-      });
-    }
+        this.selectTypeOfGuide('domainHosting');
 
-    selectTypeOfGuide(typeOfGuide) {
-      this.currentGuides = this.guides[typeOfGuide];
-      this.currentTypeOfGuide = typeOfGuide;
-    }
-  },
-);
+        return this.User
+          .getUser()
+          .then((user) => {
+            this.allGuides = _.get(
+              this.constants,
+              `urls.${user.ovhSubsidiary}.guides.all`,
+              this.constants.urls.FR.guides.all,
+            );
+          })
+          .catch(() => {
+            this.allGuides = this.constants.urls.FR.guides.all;
+          })
+          .then(() => this.unSelectProduct());
+      }
+
+      unSelectProduct() {
+        return this.WucProducts
+          .removeSelectedProduct()
+          .then((p) => {
+            this.$scope.product = p;
+          });
+      }
+
+      selectTypeOfGuide(typeOfGuide) {
+        this.currentGuides = this.guides[typeOfGuide];
+        this.currentTypeOfGuide = typeOfGuide;
+      }
+    },
+  );
