0f4e50578fe90e64bb41d09836c0d793a8182430
Frédéric Espiau <7319147+FredericEspiau@users.noreply.github.com>
2019-01-16 11:34:16 +0100

    fix(welcome): change message for FR subsidiary (#841)
    ref: MBE-159

--- client/app/configuration/configuration.controller.js
+++ client/app/configuration/configuration.controller.js
@@ -1,51 +1,41 @@
-angular.module('App').controller(
-  'ConfigurationCtrl',
-  class ConfigurationCtrl {
-    constructor(
-      $scope,
-      $route,
-      WucProducts,
-      constants,
-      User,
-    ) {
-      this.$scope = $scope;
-      this.$route = $route;
-      this.WucProducts = WucProducts;
-      this.constants = constants;
-      this.User = User;
-    }
+angular
+  .module('App')
+  .controller(
+    'configurationCtrl',
+    class ConfigurationCtrl {
+      constructor(
+        constants,
+        User,
+      ) {
+        this.constants = constants;
+        this.User = User;
+      }
 
-    $onInit() {
-      this.guides = this.constants.TOP_GUIDES;
+      $onInit() {
+        this.guides = this.constants.TOP_GUIDES;
 
-      this.User.getUser()
-        .then((user) => {
-          this.allGuides = _.get(
-            this.constants,
-            `urls.${user.ovhSubsidiary}.guides.all`,
-            this.constants.urls.FR.guides.all,
-          );
-        })
-        .catch(() => {
-          this.allGuides = this.constants.urls.FR.guides.all;
-        });
+        this.helpCenterURLs = _.omit(
+          _.mapValues(
+            this.constants.urls,
+            'support',
+          ),
+          _.isUndefined,
+        );
 
-      this.currentGuides = [];
-      this.currentTypeOfGuide = null;
+        return this.User
+          .getUser()
+          .then(({ ovhSubsidiary: subsidiary }) => {
+            this.subsidiary = subsidiary;
 
-      this.unSelectProduct();
-      this.selectTypeOfGuide('domainHosting');
-    }
-
-    unSelectProduct() {
-      this.WucProducts.removeSelectedProduct().then((p) => {
-        this.$scope.product = p;
-      });
-    }
-
-    selectTypeOfGuide(typeOfGuide) {
-      this.currentGuides = this.guides[typeOfGuide];
-      this.currentTypeOfGuide = typeOfGuide;
-    }
-  },
-);
+            this.allGuides = _.get(
+              this.constants,
+              `urls.${subsidiary}.guides.all`,
+              this.constants.urls.FR.guides.all,
+            );
+          })
+          .catch(() => {
+            this.allGuides = this.constants.urls.FR.guides.all;
+          });
+      }
+    },
+  );
