49d434eb5857a004cef395791a057b3effe59afd
varun257 <varun257@gmail.com>
2019-02-14 21:00:04 +0530

    feat(domain): add new plan tile (#918)
    ref: MBE-239

--- client/app/domain/domain-tabs.controller.js
+++ client/app/domain/domain-tabs.controller.js
@@ -28,13 +28,12 @@ angular.module('App').controller(
         'TASKS',
       ];
 
+      this.$scope.alerts = {
+        page: 'domain_alert_page',
+      };
+
       this.domain = this.$scope.ctrlDomain.domain;
 
-      const changeOwnerClassic = !_.includes(
-        this.Domain.extensionsChangeOwnerByOrder,
-        _.last(this.domain.name.split('.')),
-      );
-      const updateOwnerUrl = this.getUpdateOwnerUrl(this.domain);
 
       this.tabMenu = {
         title: this.$translate.instant('navigation_more'),
@@ -44,40 +43,6 @@ angular.module('App').controller(
             target: `#/configuration/email-domain/${
               this.domain.name
             }?tab=MAILING_LIST`,
-            type: 'LINK',
-          },
-          {
-            label: this.$translate.instant('contacts_management'),
-            target: `#/useraccount/contacts?tab=SERVICES&serviceName=${
-              this.domain.name
-            }`,
-            text: this.$translate.instant('hosting_tab_menu_contacts'),
-            type: 'LINK',
-          },
-          {
-            label: this.$translate.instant('core_change_owner'),
-            target: '',
-            text: changeOwnerClassic
-              ? ''
-              : this.$translate.instant('core_change_owner_order'),
-            type: 'EXTERNAL_LINK',
-          },
-          {
-            label: this.$translate.instant('domain_configuration_update_owner'),
-            target: updateOwnerUrl.target,
-            fn: () => this.handleOwnerUrlError(updateOwnerUrl.error),
-            type: 'ACTION',
-            disabled: !updateOwnerUrl.target,
-          },
-          {
-            type: 'SEPARATOR',
-          },
-          {
-            label: this.$translate.instant('domain_tab_menu_resiliate'),
-            target: `#/billing/autoRenew?selectedType=DOMAIN&searchText=${
-              this.domain.name
-            }`,
-            text: this.$translate.instant('hosting_tab_menu_resiliate_infos'),
             type: 'LINK',
           },
         ],
@@ -92,19 +57,10 @@ angular.module('App').controller(
       this.$q
         .all({
           domain: this.Domain.getSelected(this.$stateParams.productId),
-          changeOwnerUrl: this.User.getUrlOf(changeOwnerClassic ? 'changeOwner' : 'domainOrderChange'),
         })
-        .then(({ domain, changeOwnerUrl }) => {
+        .then(({ domain }) => {
           if (domain.managedByOvh) {
             this.tabs = _.filter(this.tabs, tab => tab !== 'DNSSEC');
-          }
-
-          if (changeOwnerClassic) {
-            this.tabMenu.items[2].target = changeOwnerUrl;
-          } else {
-            this.tabMenu.items[2].target = `${changeOwnerUrl}?domain=${
-              domain.name
-            }`;
           }
 
           if (_.isObject(domain.whoisOwner)) {
@@ -124,6 +80,11 @@ angular.module('App').controller(
           }
 
           this.setSelectedTab(angular.uppercase(this.$stateParams.tab), true);
+        }).catch(() => {
+          this.Alerter.error(
+            this.$translate.instant('domain_dashboard_loading_error'),
+            this.$scope.alerts.page,
+          );
         });
     }
 
@@ -150,33 +111,6 @@ angular.module('App').controller(
       }
 
       this.$location.search('tab', this.selectedTab);
-    }
-
-    getUpdateOwnerUrl(domain) {
-      const ownerUrlInfo = { target: '', error: '' };
-      if (_.has(domain, 'name') && _.has(domain, 'whoisOwner.id')) {
-        ownerUrlInfo.target = `#/useraccount/contact/${domain.name}/${domain.whoisOwner.id}`;
-      } else if (!_.has(domain, 'name')) {
-        ownerUrlInfo.error = this.$translate.instant('domain_tab_REDIRECTION_add_step4_server_cname_error');
-      } else {
-        switch (domain.whoisOwner) {
-          case this.DOMAIN.whoIsStatus.PENDING:
-            ownerUrlInfo.error = this.$translate.instant('domain_dashboard_whois_pending');
-            break;
-          case this.DOMAIN.whoIsStatus.INVALID_CONTACT:
-            ownerUrlInfo.error = this.$translate.instant('domain_dashboard_whois_invalid_contact');
-            break;
-          default:
-            ownerUrlInfo.error = this.$translate.instant('domain_dashboard_whois_error');
-        }
-      }
-      return ownerUrlInfo;
-    }
-
-    handleOwnerUrlError(errMsg) {
-      if (errMsg) {
-        this.Alerter.error(errMsg, this.$scope.alerts.tabs);
-      }
     }
 
     static toKebabCase(str) {
