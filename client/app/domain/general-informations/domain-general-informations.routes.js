db1676f80328f4b22b926e5e48ab4628aa6d3861
Marie JONES <marie.jones@corp.ovh.com>
2019-03-14 13:21:56 +0100

    feat(domain): replace wuc-ovh-tabs with oui-header-tabs
    Allows to use states
    MANAGER-2343

--- client/app/domain/general-informations/domain-general-informations.routes.js
+++
@@ -1,7 +0,0 @@
-angular.module('App').config(($stateProvider) => {
-  $stateProvider.state('app.domain.general-informations', {
-    url: '/informations',
-    templateUrl: 'domain/general-informations/domain-general-informations.html',
-    controller: 'DomainTabGeneralInformationsCtrl',
-  });
-});
