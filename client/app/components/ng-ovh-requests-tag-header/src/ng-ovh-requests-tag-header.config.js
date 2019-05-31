86b3402dce76c98712989af084c3abaebbbde7d3
Cyril Biencourt <cyril.biencourt@corp.ovh.com>
2019-05-31 11:44:27 +0200

    fix: remove useless component

--- client/app/components/ng-ovh-requests-tag-header/src/ng-ovh-requests-tag-header.config.js
+++
@@ -1,6 +0,0 @@
-
-export default /* @ngInject */ ($transitions, $http) => {
-  $transitions.onStart({}, (transition) => {
-    $http.defaults.headers.common['X-Ovh-Manager-State'] = `${transition.router.stateService.current.name}-${new Date().getTime()}`;
-  });
-};
